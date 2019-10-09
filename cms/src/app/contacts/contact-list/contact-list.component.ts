33bbf5c3231033478160e66962b1c3dea31e6b18
mhousewright <mack.housewright@gmail.com>
2019-10-09 17:06:08 -0600

    Week4 Commit

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
 import { Contact } from '../contact.model';
 
@@ -11,7 +11,8 @@ export class ContactListComponent implements OnInit {
   @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     new Contact(1, 'Bro. Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
-    new Contact(2, 'Bro. Barzee', 'barzeer@byui.edu', '208-496-3768', 'https://web.byui.edu/Directory/Employee/barzeer.jpg', null)
+    new Contact(2, 'Bro. Barzee', 'barzeer@byui.edu', '208-496-3768', 'https://web.byui.edu/Directory/Employee/barzeer.jpg', null),
+    new Contact(3, 'Bro. Thayne', 'thayneti@byui.edu', '208-496-3777', 'https://web.byui.edu/Directory/Employee/thayneti.jpg', null)
   ]
 
   constructor() { }
--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document(1, 'How to Be a Better Person', 'How do you be a good person? Dont do anything stupid', null, null),
+    new Document(2, 'Krusty Krab Employee Handbook', 'Remember POOP: People Order Our Patties', null, null),
+    new Document(3, 'Tacos', 'Ingredients: Taco meat, taco seasoning, taco lettuce, taco cheese', null, null),
+    new Document(4, 'How to Be a Better Person 2', 'Same thing; Dont do anything stupid', null, null),
+  ]
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
